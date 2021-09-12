809dd766abe1468693ea51293742dd01ed9368ce
justinho2100 <justinho2100@gmail.com>
2021-09-12 23:46:57 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,14 +1,14 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: '9cfed09e8b794657b31658f10d259f46',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
+  name: 'justin',
+  domain: 'betterleaf.blog',
   author: 'Travis Fischer',
 
   // open graph metadata (optional)
